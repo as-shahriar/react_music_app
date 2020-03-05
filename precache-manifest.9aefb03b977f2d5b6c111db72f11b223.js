0699201a7a6d7ec03a81b29556dc4fa23b79dc3c
Asif Shahriar <a.shahriar1999@gmail.com>
2020-03-05 20:33:41 +0600

    initial commit

---
+++ precache-manifest.9aefb03b977f2d5b6c111db72f11b223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c850fca97d4c3f36c4a4b91ca7c7e99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2068b97aa0105d989dee",
+    "url": "./static/css/main.8ca520d3.chunk.css"
+  },
+  {
+    "revision": "34a7829a12596e9637be",
+    "url": "./static/js/2.8eddc988.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.8eddc988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2068b97aa0105d989dee",
+    "url": "./static/js/main.7fbde3c6.chunk.js"
+  },
+  {
+    "revision": "677ef2574cb4ebae33cc",
+    "url": "./static/js/runtime-main.6472d833.js"
+  }
+]);
